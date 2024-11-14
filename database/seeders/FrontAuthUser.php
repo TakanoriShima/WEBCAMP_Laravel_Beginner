13da67e9a398368975ef4c0e21fa9f40520d185d
TakanoriShima <quark2galaxy@gmail.com>
2024-11-14 09:55:50 +0000

    認証と認可の実装まで書いた

---
+++ database/seeders/FrontAuthUser.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class FrontAuthUser extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'テストユーザ',
+            'email' => 'hoge@example.com',
+            'email_verified_at' => date('Y-m-d H:i:s'),
+            'password' => Hash::make('pass'),
+        ]);
+        DB::table('users')->insert([
+            'name' => '島尚徳',
+            'email' => 'shima@gmail.com',
+            'email_verified_at' => date('Y-m-d H:i:s'),
+            'password' => Hash::make('dmm'),
+        ]);       
+    }
+}
